fd3e3f750e0761925c9b6b56e56da0cee3e8a51b
hareesha <hareesha1906@gmail.com>
2017-06-28 11:40:33 -0700

    tst

---
+++ src/main/java/com/example/SpringBootConcept/SpringBootConceptApplication.java
@@ -0,0 +1,12 @@
+package com.example.SpringBootConcept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConceptApplication.class, args);
+	}
+}
