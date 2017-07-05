288a506d4e9f2c375e75b92dca66328174906474
hareesha <hareesha1906@gmail.com>
2017-07-05 10:20:29 -0700

    update

--- src/main/java/com/example/SpringBootConcept/SpringBootConceptApplication.java
+++ src/main/java/com/example/SpringBootConcept/SpringBootConceptApplication.java
@@ -2,11 +2,17 @@ package com.example.SpringBootConcept;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootConceptApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootConceptApplication.class, args);
+		
+		//Getting a reference to ApplicationContext
+		ConfigurableApplicationContext ctx = 
+				SpringApplication.run(SpringBootConceptApplication.class, args);
+		
+		
 	}
 }
