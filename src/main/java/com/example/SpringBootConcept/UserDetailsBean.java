7771c5715578db52aa6cc90f34bce6a9d8b65cc1
hareesha <hareesha1906@gmail.com>
2017-07-05 10:32:38 -0700

    demo of using ConfigurationProperties annotaion

---
+++ src/main/java/com/example/SpringBootConcept/UserDetailsBean.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.example.SpringBootConcept;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Hareesha
+ * Jul 5, 2017
+ */
+
+//Adding this annotaions lets Spring picks up this class
+@Component
+
+//Addng this enable the mapping of variables of POJO to aplication.properties file
+@ConfigurationProperties(prefix="test",ignoreInvalidFields=true)
+public class UserDetailsBean {
+	
+	String name;
+	String psw;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPsw() {
+		return psw;
+	}
+	public void setPsw(String psw) {
+		this.psw = psw;
+	}
+	
+	@Override
+	public String toString() {
+		return "UserDetailsBean [name=" + name + ", psw=" + psw + "]";
+	}
+	
+
+}
